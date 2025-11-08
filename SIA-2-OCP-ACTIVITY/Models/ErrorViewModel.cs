6ef9e9efd79eedbcf02367d4699fef563040f508
PROGLAB-27 <PROGLAB-27@PROGLAB-27>
2025-11-08 14:58:21 +0800

    Add project files.

---
+++ SIA-2-OCP-ACTIVITY/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIA_2_OCP_ACTIVITY.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
