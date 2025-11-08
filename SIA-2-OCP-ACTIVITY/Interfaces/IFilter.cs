bf0fffe3dbff1dbdd70f2654525b57b74b72c2a1
julius <julius.lajom.college@ustangelicum.edu.ph>
2025-11-08 15:27:37 +0800

    Added Shit

---
+++ SIA-2-OCP-ACTIVITY/Interfaces/IFilter.cs
@@ -0,0 +1,7 @@
+﻿namespace SIA_2_OCP_ACTIVITY.Interfaces
+{
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+}
---
+++ SIA-2-OCP-ACTIVITY/Interfaces/ISpecification.cs
@@ -0,0 +1,9 @@
+﻿using SIA_2_OCP_ACTIVITY.Models;
+
+namespace SIA_2_OCP_ACTIVITY.Interfaces
+{
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(Product p);
+    }
+}
